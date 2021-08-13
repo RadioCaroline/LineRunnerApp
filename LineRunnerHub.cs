00f2e185f5b1970415043dcc0ad475987eac725c
Lenar Shafigullin <len.shaf@gmail.com>
2021-08-13 17:00:19 +0300

    Initial commit

---
+++ LineRunnerHub.cs
@@ -0,0 +1,24 @@
+﻿using LineRunnerApp.Models;
+using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace LineRunnerApp
+{
+    /// <summary>
+    /// Хаб обработки команд приложения
+    /// </summary>
+    public class LineRunnerHub : Hub
+    {
+        /// <summary>
+        /// Добавление точки на пространство
+        /// </summary>
+        /// <param name="X"></param>
+        /// <param name="Y"></param>
+        /// <returns></returns>
+        public async Task AddPoint(double X, double Y)
+        {
+            // Отправляем команду Добавить точку для всех клиентов
+            await Clients.All.SendAsync("addPoint", X, Y);
+        }
+    }
+}
---
+++ Models/PointModel.cs
@@ -0,0 +1,12 @@
+﻿namespace LineRunnerApp.Models
+{
+    /// <summary>
+    /// Класс для работы с координатами
+    /// </summary>
+    public class Point
+    {
+        public double X { get; set; }
+
+        public double Y { get; set; }
+    }
+}
